5842337569d16e608594e771f94247b59f788c65
kahija <ouarezki.kahina@gmail.com>
2019-04-09 16:58:50 +0200

    footerScrub

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Components/Footer.css';
+import Footer from './Components/Footer';
+
+class App extends Component{
+
+  render() {
+    return (  
+      <div className="App">
+      <Footer />
+      
+      </div>
+    );
+  }     
+}
+
+export default App;
