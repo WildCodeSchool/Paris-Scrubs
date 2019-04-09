b5e3ff2a5d20e4bb4db582e0d289e25e9075a761
quintarion <quintarion@gmail.com>
2019-04-09 13:23:45 +0200

    Month profile responsive

--- paris-scrubs/src/components/MonthProfile.js
+++ paris-scrubs/src/components/MonthProfile.js
@@ -1,35 +1,36 @@
 import React from 'react';
+import eyeLogo from '../eyeLogo.svg';
 import './MonthProfile.css';
 
 const profiles = [
     {
     photo :"https://images.unsplash.com/photo-1516651000622-7f32fe80a57a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
     firstName : "Nolan",
-    default : "Aquaphobie ou la peur de l'eau, ne se lave jamais",
+    flaw : "Aquaphobie ou la peur de l'eau et ne se lave jamais",
     button : "Voir le profil de ", 
     },
     {
     photo :"https://images.unsplash.com/photo-1528960393052-3d5a27ac0cf2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=751&q=80",
     firstName : "Inès",
-    default : "Complexée par sa face gauche",
+    flaw : "Complexée par sa face gauche",
     button : "Voir le profil de ",
     },
 ]
 
 const MonthProfile = () => (
-    <div className="container">
+    <div className="monthContainer">
         {profiles.map(profile => (
         <div>
-            <div className="photoContainer">
-                <img src={profile.photo} alt="photo" className="photo"/>
+            <div className="monthBox">
+                <img src={profile.photo} alt="photo" className="monthPhoto"/>
+                <p className="monthTitle">SUPER DEFAUT DU MOIS</p>
             </div>
         <br />
-        <span className="name"><strong>{profile.firstName}</strong></span>
+        <span className="monthFlaw">{profile.flaw}</span>
         <br />
-        <span className="default">{profile.default}</span>
+        <span className="monthName"><strong>{profile.firstName}</strong></span>
         <br />
-        <br />
-        <button className="button">{profile.button}{profile.firstName}</button>
+        <a href=""><img src={eyeLogo} className="monthLogo" alt="logo" /></a>
         </div>
         ))}
     </div>
