209a128cbe0a3a424fc36f5d1a108ba69f1ef9bd
matteo farigu <matteo.farigu@gmail.com>
2019-04-11 11:52:00 +0200

    patch-pictograms-json

--- paris-scrubs/src/App.js
+++ paris-scrubs/src/App.js
@@ -1,44 +1,27 @@
 import React, { Component } from 'react';
 import Displaypicto from './components/Displaypicto'
-
-import './App.css';
-HEAD
 import MonthProfile from './components/MonthProfile';
 import Navbar from './components/navbar';
-
-import MonthProfile from './components/MonthProfile';
-import Navbar from './components/navbar';
-
 import Card from './components/Cards.js'
 
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         
-        <Displaypicto  />
-        <header >
-          
-        </header>
-        <Card />
         <header className="App-header">
-
           <Navbar />
-          
-          
         </header>
-
+          
+        <Displaypicto  />
         
+        <article>
+          <MonthProfile />
+        </article>
 
-        <header >
-          
-        </header>
-          <article>
-            <MonthProfile />
-          </article>
-      <Card />
-      
+         <Card />
       </div>
     );
   }
