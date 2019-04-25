941ba280780191a5d08970d564668ac205bb3e46
quintarion <quintarion@gmail.com>
2019-04-25 15:20:57 +0200

    review ProfilCard and three button home page

--- paris-scrubs/src/components/ThreeButton.js
+++ paris-scrubs/src/components/ThreeButton.js
@@ -4,13 +4,13 @@ import './ThreeButton.css';
 
 const buttons = [
     {
-    man : [ <Link to ="/profilepage"><button className="threeButton" type="button">Je cherche un homme</button></Link>],
+    man : [ <Link to ="/profilepage"><img src="https://image.noelshack.com/fichiers/2019/17/4/1556190915-men.jpg" alt="" className="threeButton"/></Link>],
     },
     {
-    woman : [ <Link to ="/profilepage"><button className="threeButton" type="button">Je cherche une femme</button></Link>],
+    woman : [ <Link to ="/profilepage"><img src="https://image.noelshack.com/fichiers/2019/17/4/1556190964-woman.jpg" alt="" className="threeButton"/></Link>],
     },
     {
-    other : [ <Link to ="/profilepage"><button className="threeButton" type="button">Je cherche autre chose</button></Link>],
+    other : [ <Link to ="/profilepage"><img src="https://image.noelshack.com/fichiers/2019/17/4/1556191008-others.jpg" alt="" className="threeButton"/></Link>],
     },
 ]
 
