f94361053b24c5e0b30378ce511de2249d96144b
Discaz <olivier.discazeaux@gmail.com>
2019-04-26 17:11:36 +0200

    minor changes

--- paris-scrubs/src/components/DisplayProfiles/PageProfileDisplayOther.js
+++ paris-scrubs/src/components/DisplayProfiles/PageProfileDisplayOther.js
@@ -9,7 +9,7 @@ const PageProfileDisplayOther = () => {
 
     <div className="gridcapacity">
     {PageProfiles.filter(PageProfile => PageProfile.gender.includes("autre")).map((PageProfile,index) => (< PageProfileCard default1={PageProfile.physic} default2={PageProfile.mental} name={PageProfile.username} img={PageProfile.img} sex={PageProfile.sex}/>))}
- )} 
+  
 
   
     </div>
