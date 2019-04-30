e33e93fa341e3d9b869da580e7d49660b7f8272e
quintarion <quintarion@gmail.com>
2019-04-30 17:58:31 +0200

    mediaqueries 320px

--- paris-scrubs/src/components/Cards.js
+++ paris-scrubs/src/components/Cards.js
@@ -7,16 +7,14 @@ import './Cards.css';
 
 const Card = () => {
     return (
-<body className="cardsBody">
+<div className="cardsBody">
 <h1 id="nostemoins">Nos témoignages</h1>
     <div className="cardsAllcards">
         {infosCard.map(infoCard => <DisplayCard default={infoCard.default} name={infoCard.name} img={infoCard.img} desc={infoCard.desc}/>
  )}
 
-        
-    </div>
-            
-</body>
+    </div>      
+</div>
       
     )
 }
