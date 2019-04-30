ec1ca45cd6a2e886b3b265746f5d55561f9473d8
Discaz <olivier.discazeaux@gmail.com>
2019-04-30 16:42:38 +0200

    new profiles pages with API profile in Json

--- paris-scrubs/src/components/DisplayProfiles/PageProfileDisplay.js
+++ paris-scrubs/src/components/DisplayProfiles/PageProfileDisplay.js
@@ -1,22 +1,50 @@
-import React from "react";
-import PageProfiles from "./PageProfiles.json"
+import React, { Component } from 'react';
+
 import './PageProfileCard.css';
 import PageProfileCard from "./PageProfileCard";
 
-const PageProfileDisplay = () => {
+
+
+class PageProfileDisplay extends Component {
+    
+    state = {
+        // on peut mettre notre tableau vide par défaut
+        profiles: [],
+    }
+
+
+    getProfile = () => {
+        // Récupération des profils via fetch
+        fetch("https://discaz.github.io/API-profiles/api/all.json")
+          .then(response  =>  response.json())
+          .then(result  => console.log(result) ||
+            // mise à jour de la state avec les nouvelles données
+            this.setState({
+              profiles:  result 
+            }))
+        };
+    
+    componentDidMount() {
+        this.getProfile()
+      }
+
+
+   render() {
+    const { profiles } = this.state
+    
     return (
 <body  className="cardsBody">
 
     <div className="gridcapacity">
-        {PageProfiles.map(PageProfile => < PageProfileCard default1={PageProfile.physic} default2={PageProfile.mental} name={PageProfile.username} img={PageProfile.img} sex={PageProfile.sex}/>
- )} 
+        {profiles.map(profile => < PageProfileCard default1={profile.physic} default2={profile.mental} name={profile.username} img={profile.img} sex={profile.sex}/>)} 
 
         
     </div>
             
 </body>
-      
-    )
+    );
 }
+}      
+
 
 export default PageProfileDisplay ;
